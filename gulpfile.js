840c93346a20ba0b900a243b43ac9b96c965f72c
Camron <caakhave@hotmail.com>
2018-11-09 14:22:40 -0500

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = ['components/scripts/pixgrid.js',
+var jsSources = ['components/scripts/tagline.js',
+                 'components/scripts/pixgrid.js',
                  'components/scripts/rclick.js',
-                 'components/scripts/tagline.js',
                  'components/scripts/template.js'
 ];
 
@@ -20,5 +21,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
