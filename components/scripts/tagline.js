5e1316fd057586bf0dff36fc429939f7d248ef39
Camron <caakhave@hotmail.com>
2018-11-09 12:49:16 -0500

    Added coffeescript files

--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -1,5 +1,7 @@
-var $;
+var fill;
 
-$ = require('jquery');
+(fill = function(item) {
+  return $('.tagline').append("${item}");
+})('The most creative minds in Art');
 
-$('.tagline').append("The most creative minds in art.");
+fill;
